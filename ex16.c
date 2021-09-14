6ed038f84bccbdef8e366e6e361290695c1d5d57
rodrigosuelli <rodrigosuellli@gmail.com>
2021-09-14 11:29:46 -0300

    Add ex16

--- ex16.c
+++ ex16.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <math.h>
 
 #include "get_input.h"
 
 int main()
 {
+  for (int i = 0; i <= 10; i++)
+  {
+    printf("2 elevado a %d = %.0f\n", i, powf(2, i));
+  }
+
   return 0;
 }
