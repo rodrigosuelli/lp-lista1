b4b13960f07174baef6dc56ddfb6ac8c5ac82863
rodrigosuelli <rodrigosuellli@gmail.com>
2021-09-14 11:26:36 -0300

    Add ex15.c

---
+++ ex15.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "get_input.h"
+
+int main()
+{
+  for (int i = 1; i <= 10; i++)
+  {
+    for (int j = 1; j <= 10; j++)
+    {
+      printf("%d * %d = %d\n", i, j, i * j);
+    }
+    puts("===================================");
+  }
+
+  return 0;
+}
---
+++ ex16.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "get_input.h"
+
+int main()
+{
+  return 0;
+}
