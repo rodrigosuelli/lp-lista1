5d43d0661d0a4905fe0d9e0d395a9568d2513227
rodrigosuelli <rodrigosuellli@gmail.com>
2021-09-14 11:36:41 -0300

    Add ex14

--- ex14.c
+++ ex14.c
@@ -4,5 +4,24 @@
 
 int main()
 {
+  float a, b, c;
+
+  a = get_positive_float("Insira o valor de a:");
+  b = get_positive_float("Insira o valor de b:");
+  c = get_positive_float("Insira o valor de c:");
+
+  if (a == b && b == c)
+  {
+    puts("equilatero");
+  }
+  else if (a == b || a == c || b == c)
+  {
+    puts("isosceles");
+  }
+  else
+  {
+    puts("escaleno");
+  }
+
   return 0;
 }
